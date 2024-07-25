7059677e80dd26943d49ac8b3c253793b16897e5
Jon de M <roine@users.noreply.github.com>
2024-07-25 17:45:31 +1000

    Create from-vite.yml
    Create a github page

--- src/router.tsx
+++ src/router.tsx
@@ -3,18 +3,21 @@ import ArtworkList from "./features/artworks/components/ArtworkList.tsx";
 import AppLayout from "./AppLayout.tsx";
 import { ErrorElement } from "./ErrorElement.tsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <AppLayout />,
-    errorElement: <ErrorElement />,
-    children: [
-      {
-        index: true,
-        element: <ArtworkList />,
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <AppLayout />,
+      errorElement: <ErrorElement />,
+      children: [
+        {
+          index: true,
+          element: <ArtworkList />,
+        },
+      ],
+    },
+  ],
+  { basename: "/bef8b930-832c-4d47-8425-b259f0dcf3f1" },
+);
 
 export default router;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/bef8b930-832c-4d47-8425-b259f0dcf3f1",
+});
